d7185b21c215fbcd279680196fbf5fd49288aab4
Milton <milton761@gmail.com>
2021-10-21 11:44:20 -0500

    to test individual tests regex with autograding

---
+++ src/test/java/datastructures/DoubleLinkedListTest.java
@@ -0,0 +1,19 @@
+package datastructures;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DoubleLinkedListTest {
+
+    @Test
+    public void ddl_1_test() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void ddl_2_test() {
+        Assert.assertTrue(false);
+    }
+
+
+}
